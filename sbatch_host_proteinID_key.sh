ff8ce9c30a7fc2efb0fb529921e8fb7254e0a692
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2026-01-28 11:50:10 -0800

    sbatch

---
+++ sbatch_host_proteinID_key.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -D /home/ajmuhich/
+#SBATCH -o /home/ajmuhich/slurm-log/host_proteinID_key_stdout-%j.txt
+#SBATCH -e /home/ajmuhich/slurm-log/host_proteinID_key_stderr-%j.txt
+#SBATCH -J host_proteinID_key
+#SBATCH -t 2-00:00:00
+#SBATCH --mem 50G
+#SBATCH -n 8
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ajmuhich@ucdavis.edu
+
+module load R
+
+# Define input file paths
+INPUT_FILE=$1
+COLUMN_NAME=$2
+OUTPUT_FILE=$3
+
+Rscript ~/solanaceae/host_proteinID_key.R \
+"$COUNTS_FILE1" "$COUNTS_FILE2" \
+"$OUTPUT_DIR"
