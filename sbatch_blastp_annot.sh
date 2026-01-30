1a2779e128f87920de6d437eaa426adba963bd1a
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2026-01-30 13:54:15 -0800

    outfiles

--- sbatch_blastp_annot.sh
+++ sbatch_blastp_annot.sh
@@ -18,14 +18,16 @@ fi
 
 QUERY="$1"
 DB="$2"
-OUTPUT_TSV="$3"
+OUTPUT_CSV="$3"
 
 mkdir -p $OUTPUT_DIR
 
 blastp \
     -query $QUERY \
     -db $DB \
-    -out $OUTPUT_TSV \
+    -out raw_out.tsv \
     -evalue 1e-5 \
     -max_target_seqs 1 \
     -outfmt "6 qseqid sseqid pident length qcovs evalue bitscore stitle"
+
+Rscript ~/solanaceae/blastp_reformat.R raw_out.tsv $DB $OUTPUT_CSV
